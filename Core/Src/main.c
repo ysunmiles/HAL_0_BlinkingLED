cde55415a7f5503142dfc5ddd835db51ae0d1973
ysunmiles <ysunmiles@gmail.com>
2026-05-29 15:31:46 +0200

    添加 LED 闪烁示例代码及readme文件

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -94,6 +94,9 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+      HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+      HAL_Delay(500);
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
